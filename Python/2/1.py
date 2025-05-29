25ad5ca8ceedfd87dfb47a085369ae1cd95f4dd9
rahirea <100532495+rahirea@users.noreply.github.com>
2025-05-29 20:35:30 +0300

    1.py

--- Python/2/1.py
+++ Python/2/1.py
@@ -1,4 +1,3 @@
-# Ввод списка элементов
 elements = input("Введите числа через пробел: ").split()
 power = int(input("Введите степень: "))
 
@@ -6,15 +5,11 @@ result = []
 
 for element in elements:
     try:
-        # Пробуем преобразовать элемент в число
         num = float(element)
-        # Проверяем, является ли число целым (для красивого вывода)
         if num.is_integer():
             num = int(num)
-        # Возводим в степень
         result.append(str(num ** power))
     except ValueError:
-        # Если не число - умножаем строку на степень
         result.append(element * power)
 
 print("Вывод:", " ".join(result))
